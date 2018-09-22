c931b090a0a7c3b8943d777cd904e7201c951e47
mldn <A1754734665@163.com>
2018-09-22 19:47:38 +0800

    增加了Emp.java文件

---
+++ src/Emp.java
@@ -0,0 +1,8 @@
+public class  Emp implements java.io.Serializable{
+	private Integer empno ;
+	private String ename ;
+	public String toString() {
+		System.out.println("只是没事干") ;
+		return "" ;
+	}
+}
